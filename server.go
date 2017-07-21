73ccd30570dcc5ce9c66c33b4b184be6376bae78
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-07-21 10:06:40 -0700

    Now making this into a chat server that's not serving static files

--- server.go
+++ server.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
 
+type Chats struct {
+	ID string `json:"id,omitempty"`
+	Timestamp string `json:"timestamp,omitempty"`
+	Username string `json:username,omitempty`
+	Message string `json:"message,omitempty"`
+	Group string `json:"group,omitempty"`
+}
+
+var chatCache []Chats
+
 func main() {
+	chatCache = append(chatCache, Chats{ID: "1", Timestamp: "2017-07-21", Username: "Tom", Message: "Hello all", Group: "lobby"})
+	chatCache = append(chatCache, Chats{ID: "2", Timestamp: "2017-07-21", Username: "Ebba", Message: "Hey brother", Group: "lobby"})
 	http.HandleFunc("/", defaultPath)
 	http.HandleFunc("/api/data", returnData)
-	
 	fmt.Println("Server is listening to port 1337")
 	http.ListenAndServe(":1337", nil)
 }
@@ -20,5 +32,5 @@ func defaultPath(w http.ResponseWriter, r *http.Request) {
 
 func returnData(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("A web user requested the /data path \n")
-	fmt.Fprintf(w, "Your request was succesfull, I will get you your data \n")
+	json.NewEncoder(w).Encode(chatCache)
 }
